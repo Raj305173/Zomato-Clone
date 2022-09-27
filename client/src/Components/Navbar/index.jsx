dd3d169774db3f86b61dfa1e1e737e81be767b81
Raj Agrawal <krishagrawal343@gmail.com>
2022-09-27 16:36:49 +0530

    backendcomplete

---
+++ client/src/Components/Navbar/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar=()=>{
+return <>
+<nav className="">
+<div>
+    <img src="https://b.zmtcdn.com/web_assets/8313a97515fcb0447d2d77c276532a511583262271.png" alt="logo" className="w-full h-full" />
+</div>
+</nav>
+</>
+};
+export default Navbar;
